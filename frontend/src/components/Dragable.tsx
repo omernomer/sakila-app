b7280ad89ef9654a0616d5fc12f58a4bada9ec83
Omar Al-Debagy <omeraldebagy@gmail.com>
2025-02-23 18:04:58 +0100

    add Draggable component for drag-and-drop functionality

---
+++ frontend/src/components/Dragable.tsx
@@ -0,0 +1,24 @@
+import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
+import { ReactNode } from "react";
+
+interface DraggableProps {
+  id: string;
+  children: ReactNode;
+}
+function Draggable({ id, children }: DraggableProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id,
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {children}
+    </button>
+  );
+}
+
+export default Draggable;
