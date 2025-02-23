0d406ba7d1dfae9fcc9cad776458151f8944d52c
Omar Al-Debagy <omeraldebagy@gmail.com>
2025-02-23 20:06:38 +0100

    implement routing with React Router: add MainPage and Charts components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
+import Charts from "./Charts";
 import Users from "./Users";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
-    <>
-      {/* <DragAndDrop /> */}
-      <Users />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/charts" element={<Charts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
