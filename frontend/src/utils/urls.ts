c90491bdead3bac4ef59fb82636fd1f96e4c4e6a
Omar Al-Debagy <omeraldebagy@gmail.com>
2025-02-23 20:06:15 +0100

    add new API endpoints for categories count and most rented films

--- frontend/src/utils/urls.ts
+++ frontend/src/utils/urls.ts
@@ -1,3 +1,5 @@
 const BACKEND_URL = 'http://localhost:3000';
 export const USERS_URL = `${BACKEND_URL}/users`;
 export const UPDATE_USER_STATUS_URL = `${BACKEND_URL}/update-user-status`;
+export const CATEGORIES_COUNT_URL = `${BACKEND_URL}/categoriesCount`;
+export const MOST_RENTED_FILMS_URL = `${BACKEND_URL}/mostRentedFilms`;
