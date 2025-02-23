28b5314c2b5c1cf7b1c0f7f74b5bbd3c1101476b
Omar Al-Debagy <omeraldebagy@gmail.com>
2025-02-23 20:08:48 +0100

    add endpoints for categories count and most rented films; refactor user status update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express, { Application } from "express";
 
 const app: Application = express();
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 // Middleware
 app.use(cors());
@@ -19,8 +18,8 @@ app.get("/", (req, res) => {
 app.get("/users", async (req, res) => {
   const users = await prisma.customer.findMany({
     orderBy: {
-      last_update: 'desc'
-    }
+      last_update: "desc",
+    },
   });
   const activeUsers = users.filter((user: any) => user.active);
   const inactiveUsers = users.filter((user: any) => !user.active);
@@ -32,12 +31,56 @@ app.post("/update-user-status", async (req, res) => {
   try {
     const user = await prisma.customer.update({
       where: { customer_id: id },
-      data: { active }, 
+      data: { active },
     });
-  
+
     res.json(user);
+  } catch (error) {
+    console.log(error);
   }
-  catch (error) {
+});
+
+app.get("/categoriesCount", async (_req, res) => {
+  try {
+    const fileCategories = await prisma.film_category.findMany({});
+    const categories = await prisma.category.findMany({});
+    const categoriesCount = categories.map((category: any) => {
+      return {
+        name: category.name,
+        count: fileCategories.filter(
+          (fileCategory: any) =>
+            fileCategory.category_id === category.category_id
+        ).length,
+      };
+    });
+
+    res.json({ categoriesCount });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/mostRentedFilms", async (_req, res) => {
+  try {
+    const topRentedMovies: {
+      title: string;
+      rental_count: bigint;
+    }[] = await prisma.$queryRaw`
+      SELECT f.title, COUNT(r.rental_id) AS rental_count
+        FROM rental r
+        JOIN inventory i ON r.inventory_id = i.inventory_id
+        JOIN film f ON i.film_id = f.film_id
+        GROUP BY f.title
+        ORDER BY rental_count DESC
+        LIMIT 10;
+    `;
+    const topRentedFilms = topRentedMovies?.map((item) => ({
+      title: item.title,
+      rentalCount: Number(item.rental_count), // Convert BigInt to Number
+    }));
+
+    res.json({ topRentedFilms });
+  } catch (error) {
     console.log(error);
   }
 });
